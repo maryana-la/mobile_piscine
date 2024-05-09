e791b59c47f36c195a35a1803ddcceba0d5a771d
rchelsea <rchelsea@student.42.fr>
2024-05-09 19:31:37 +0200

    Initial commit

---
+++ ex00/app/src/main/java/com/mobile/ex00/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.mobile.ex00
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    val mainLabel = findViewById<TextView>(R.id.mainLabel)
+    val button = findViewById<Button>(R.id.button)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            println("Button pressed")
+            Toast.makeText(this, "Thanks for clicking", Toast.LENGTH_SHORT).show()
+        }
+
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+}
