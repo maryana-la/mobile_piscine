140efe5c2b400e69048149cc315ff9114f81186d
maryana <rchelsea@student.42.fr>
2025-02-18 18:55:08 +0100

    day03 started, searchview and background added

---
+++ day03/mobileWeatherApp/app/src/main/java/com/mobilepiscine42/mediumweatherapp/MainActivity.kt
@@ -0,0 +1,214 @@
+package com.mobilepiscine42.advanced_weather_app
+
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.Location
+import android.location.LocationManager
+import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.SearchView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mobilepiscine42.advanced_weather_app.geocoding_api.GeocodingViewModel
+import com.mobilepiscine42.advanced_weather_app.geocoding_api.Result
+import com.mobilepiscine42.advanced_weather_app.pageviewer.SharedViewModel
+import com.mobilepiscine42.advanced_weather_app.pageviewer.ViewPagerAdapter
+import com.mobilepiscine42.advanced_weather_app.reverse_geocoding_api.ReverseGeoViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var locationManager: LocationManager
+    private lateinit var weatherViewModel : WeatherViewModel
+    private lateinit var geocodingViewModel : GeocodingViewModel
+    private lateinit var reverseGeoViewModel : ReverseGeoViewModel
+    private lateinit var sharedViewModel : SharedViewModel
+    private lateinit var searchView : SearchView
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: CitySuggestionAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        setPageView()
+        setLocationService()
+        setSearchView()
+        weatherViewModel = ViewModelProvider(this)[WeatherViewModel::class.java]
+        geocodingViewModel = ViewModelProvider(this)[GeocodingViewModel::class.java]
+        sharedViewModel = ViewModelProvider(this)[SharedViewModel::class.java]
+        reverseGeoViewModel = ViewModelProvider(this)[ReverseGeoViewModel::class.java]
+        sharedViewModel.cityOptionsLiveData.observe(this) {
+            Log.i("Cities", sharedViewModel.getCityOptions().toString())
+            adapter.updateSuggestions(sharedViewModel.getCityOptions())
+        }
+    }
+
+    private fun setPageView() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val viewPager = findViewById<ViewPager2>(R.id.viewPager2)
+        val adapter = ViewPagerAdapter(this)
+        viewPager.adapter = adapter
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                bottomNavigationView.menu.getItem(position).isChecked = true
+            }
+        })
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.currently -> viewPager.currentItem = 0
+                R.id.today -> viewPager.currentItem = 1
+                R.id.weekly -> viewPager.currentItem = 2
+                else -> viewPager.currentItem = 0
+            }
+            true
+        }
+    }
+    private fun setLocationService() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    }
+
+    private fun setSearchView() {
+        searchView = findViewById(R.id.searchGeoText)
+        searchView.clearFocus()
+        searchView.isIconified = false
+
+
+//        searchView.queryHint = "1Search location"
+//        searchView.queryHint = HtmlCompat.fromHtml("<font color = #FAEADCDC>" + getString(R.string.hintSearchView) + "</font>", HtmlCompat.FROM_HTML_MODE_LEGACY)
+
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        adapter = CitySuggestionAdapter(mutableListOf()) { city -> onCitySelected(city) }
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let {
+                    Log.i ("City name : ", query)
+                    Log.e ("OnQueryTextSubmit", "${query}, log")
+                    geocodingViewModel.getData(query, sharedViewModel)
+
+                    searchView.setQuery("", false)
+                    searchView.clearFocus()
+                    recyclerView.visibility = View.GONE
+                    val tmp = sharedViewModel.getCityOptions()
+                    if (tmp.size != 0) {
+                       onCitySelected(tmp[0])
+                    } else {
+                        sharedViewModel.setErrorMsg("Could not find any result for the provided address or coordinates.")
+                    }
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText == null || newText.length < 3)  {
+                    Log.e ("OnQueryTextChange", "${newText}, log")
+                    adapter.updateSuggestions(emptyList())
+                    recyclerView.visibility = RecyclerView.GONE
+                    return true
+                }
+                newText.let { query ->
+                    if (query.isNotBlank()) {
+                        recyclerView.visibility = RecyclerView.VISIBLE
+                        geocodingViewModel.getData(query, sharedViewModel)
+                    } else {
+                        searchView.clearFocus()
+                        adapter.updateSuggestions(emptyList())
+                        recyclerView.visibility = View.GONE
+                    }
+                }
+                return true
+            }
+        })
+
+        searchView.setOnCloseListener {
+            Log.e("onCLoseListener", "log")
+            searchView.clearFocus()
+            adapter.updateSuggestions(emptyList())
+            recyclerView.visibility = View.GONE
+            true
+        }
+    }
+
+    private fun onCitySelected(city: Result) {
+        weatherViewModel.getData(city.latitude.toString(), city.longitude.toString(), sharedViewModel, reverseGeoViewModel)
+        searchView.setQuery("", false)
+        searchView.clearFocus()
+        sharedViewModel.setCityOptions(emptyList())
+        recyclerView.visibility = View.GONE
+        Log.i("RecycleView","Selected City: ${city.name}")
+    }
+
+    fun requestLocation(view : View) {
+        if (checkLocationPermission()) {
+            getGPS()
+        } else {
+            requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getGPS() {
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Log.i("GPS", "GPS is disabled! Please enable it in settings.")
+            sharedViewModel.setErrorMsg("GPS is disabled.\nPlease turn it on in settings and try again.")
+            return
+        }
+
+        locationManager.getCurrentLocation(
+            LocationManager.GPS_PROVIDER,
+            null,
+            mainExecutor
+        ) { location: Location? ->
+            if (location != null) {
+                val latitude = location.latitude.toString()
+                val longitude = location.longitude.toString()
+                weatherViewModel.getData(latitude, longitude, sharedViewModel, reverseGeoViewModel)
+                Log.d("GPS", "latitude: $latitude, longitude: $longitude")
+            } else {
+                Log.i("GPS", "Error getting GPS location!")
+                sharedViewModel.setErrorMsg("Cannot get location from GPS.\nPlease try again later.")
+            }
+        }
+    }
+
+    private fun checkLocationPermission() : Boolean {
+        return ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (!isGranted) {
+            sharedViewModel.setErrorMsg("Location permission is not granted.\nPlease enable GPS access in the settings.")
+            Log.i("Location permission", "Location permission denied")
+        } else {
+            getGPS()
+        }
+    }
+}
+
+
+
---
+++ day03/mobileWeatherApp/app/src/main/java/com/mobilepiscine42/mediumweatherapp/pageviewer/Currently.kt
@@ -0,0 +1,71 @@
+package com.mobilepiscine42.advanced_weather_app.pageviewer
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.mobilepiscine42.advanced_weather_app.R
+
+class Currently : Fragment() {
+    private lateinit var sharedViewModel: SharedViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_currently, container, false)
+        val mainLayout = view?.findViewById<LinearLayout>(R.id.mainLayout)
+        val city = view?.findViewById<TextView>(R.id.city)
+        val region = view?.findViewById<TextView>(R.id.region)
+        val country = view?.findViewById<TextView>(R.id.country)
+        val temperature = view?.findViewById<TextView>(R.id.temperature)
+        val wind = view?.findViewById<TextView>(R.id.wind)
+        val errorMessage = TextView(context)
+        sharedViewModel = ViewModelProvider(requireActivity())[SharedViewModel::class.java]
+
+        sharedViewModel.cityLiveData.observe(viewLifecycleOwner) {
+            mainLayout?.removeView(errorMessage)
+            Util.removeErrorMessage(errorMessage)
+            city?.text = sharedViewModel.getCurrentCity().City
+            region?.text = sharedViewModel.getCurrentCity().Region
+            country?.text = sharedViewModel.getCurrentCity().CntryName
+        }
+
+        sharedViewModel.forecastLiveData.observe(viewLifecycleOwner) {
+            mainLayout?.removeView(errorMessage)
+            Util.removeErrorMessage(errorMessage)
+            temperature?.text = sharedViewModel.getWeatherForecast().current.temperature_2m.toString() +
+                sharedViewModel.getWeatherForecast().current_units.temperature_2m
+            wind?.text = sharedViewModel.getWeatherForecast().current.wind_speed_10m.toString() +
+                    sharedViewModel.getWeatherForecast().current_units.wind_speed_10m + " " +
+                    Util.getWindDirection(sharedViewModel.getWeatherForecast().current.wind_direction_10m)
+
+        }
+
+        sharedViewModel.errorLiveData.observe(viewLifecycleOwner) {
+            if (sharedViewModel.getErrorMsg().isNotEmpty()) {
+                Log.e("FRAGMENT Currently", "Error message print")
+                city?.text = ""
+                region?.text = ""
+                country?.text = ""
+                temperature?.text = ""
+                wind?.text = ""
+                mainLayout?.removeView(errorMessage)
+                if (mainLayout != null) {
+                    errorMessage.text = sharedViewModel.getErrorMsg()
+                    Util.setupErrorMessage(errorMessage, mainLayout)
+                }
+            }
+        }
+        return view
+    }
+}
